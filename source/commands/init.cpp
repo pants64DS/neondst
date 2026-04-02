593545a7b5a45e16762114405956825aa342585e
pants64DS <pants64DS@users.noreply.github.com>
2026-04-03 00:41:40 +0300

    Make the init command create all relevant directories

--- source/commands/init.cpp
+++ source/commands/init.cpp
@@ -5,8 +5,11 @@ void extract(const fs::path& ndsInputPath, const fs::path& fsOutputPath);
 void Commands::init(const fs::path& cleanRomPath)
 {
 	const fs::path clean = "clean";
+	const fs::path modified = "modified";
 
 	extract(cleanRomPath, clean / "raw");
 
 	fs::create_directory(clean / "decompressed");
+	fs::create_directories(modified / "uncompressed");
+	fs::create_directory(modified / "final");
 }
