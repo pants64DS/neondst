9cc718d5a0a2c1766d1d792faa09a4f589145fef
pants64DS <pants64DS@users.noreply.github.com>
2026-04-19 17:47:09 +0300

    Finalize the status command

--- source/commands/status.cpp
+++ source/commands/status.cpp
@@ -1,33 +1,48 @@
 #include "command.h"
 #include "config.h"
 
+#include <unordered_set>
 #include <iostream>
+
+const fs::path cleanRaw               = fs::path("clean") / "raw";
+const fs::path modifiedBase           = fs::path("modified") / "base";
+const fs::path modifiedFinal          = fs::path("modified") / "final";
+const fs::path modifiedToBeCompressed = fs::path("modified") / "to-be-compressed";
 
 struct StatusExtractor : Extractor
 {
 	using Extractor::Extractor;
+	std::unordered_set<fs::path> paths;
 	std::vector<fs::path> diffPaths;
 	std::vector<fs::path> romOnlyPaths;
 
 	virtual void writeFile(const fs::path& shortPath, const void* data, std::size_t size)
 	{
-		if (fileExistsAndEquals("modified" / ("final" / shortPath), data, size))
+		paths.insert(shortPath);
+
+		if (fileExistsAndEquals(modifiedFinal / shortPath, data, size))
 			return;
 
-		fs::path modifiedBasePath = "modified" / ("base" / shortPath);
+		fs::path modifiedBasePath = modifiedBase / shortPath;
 
 		if (fs::is_regular_file(modifiedBasePath))
 		{
 			if (!fileEquals(modifiedBasePath, data, size))
 				diffPaths.push_back(shortPath);
 		}
-		else if (!fs::is_regular_file("clean" / ("raw" / shortPath)))
+		else if (!fs::is_regular_file(cleanRaw / shortPath))
 			romOnlyPaths.push_back(shortPath);
 	}
 
 	virtual void writeDir(const fs::path& shortPath)
 	{
+		paths.insert(shortPath);
 
+		if (fs::is_directory(modifiedFinal / shortPath)) return;
+		if (fs::is_directory(modifiedBase / shortPath)) return;
+		if (fs::is_directory(cleanRaw / shortPath)) return;
+
+		romOnlyPaths.push_back(shortPath);
 	}
 };
 
@@ -38,27 +53,54 @@ void Commands::status(const fs::path& romPath)
 	StatusExtractor status {config.romPath};
 	status.extract();
 
-	if (status.romOnlyPaths.empty() && status.diffPaths.empty())
+	bool changes = false;
+	std::vector<fs::path> sourceOnlyPaths;
+
+	for (const fs::path& rootPath : {modifiedBase, modifiedToBeCompressed, modifiedFinal})
 	{
-		std::cout << "No changes\n";
-		return;
+		for (const fs::directory_entry& dirEntry : fs::recursive_directory_iterator(rootPath))
+		{
+			const fs::path& p = dirEntry.path();
+
+			auto q = relative(p, rootPath);
+
+			if (!status.paths.contains(q))
+				sourceOnlyPaths.push_back(p);
+		}
+	}
+
+	if (!sourceOnlyPaths.empty())
+	{
+		changes = true;
+		std::cout << "Files in " << fs::path("modified") << " not in " << config.romPath << ":\n";
+
+		for (const fs::path& p : sourceOnlyPaths)
+			std::cout << "\t\x1B[0;32m" << p.string() << "\033[0m\n";
+
+		std::cout << '\n';
 	}
 
 	if (!status.romOnlyPaths.empty())
 	{
+		changes = true;
 		std::cout << "Files in " << config.romPath << " not in the source directories:\n";
 
 		for (const auto& p : status.romOnlyPaths)
-			std::cout << '\t' << p << '\n';
+			std::cout << "\t\x1B[0;31m" << p.string() << "\033[0m\n";
+
+		std::cout << '\n';
 	}
 
 	if (!status.diffPaths.empty())
 	{
+		changes = true;
 		std::cout << "Files that differ between " << config.romPath << " and the source directories:\n";
 
 		for (const auto& p : status.diffPaths)
-			std::cout << '\t' << p << '\n';
+			std::cout << "\t\x1B[0;33m" << p.string() << "\033[0m\n";
+
+		std::cout << '\n';
 	}
 
-	// TODO: show files that are only in the source directories
+	if (!changes) std::cout << "No changes\n";
 }
