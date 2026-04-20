ff4b22ae0769ec965b88b25d518724461dacc0cf
pants64DS <pants64DS@users.noreply.github.com>
2026-04-20 07:33:46 +0300

    Fix console output on Windows

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,39 @@
 #include "command.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#include <iostream>
+#endif
+
 int main(int argc, char** argv)
 {
+#ifdef _WIN32
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	if (hOut == INVALID_HANDLE_VALUE)
+	{
+		std::cout << "GetStdHandle returned with error 0x";
+		std::cout << std::hex << GetLastError() << '\n';
+		return 1;
+	}
+
+	DWORD dwMode = 0;
+	if (!GetConsoleMode(hOut, &dwMode))
+	{
+		std::cout << "GetConsoleMode returned with error 0x";
+		std::cout << std::hex << GetLastError() << '\n';
+		return 1;
+	}
+
+	dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+	if (!SetConsoleMode(hOut, dwMode))
+	{
+		std::cout << "SetConsoleMode returned with error 0x";
+		std::cout << std::hex << GetLastError() << '\n';
+		return 1;
+	}
+#endif
+
 	if (argc < 2)
 	{
 		Commands::help();
